a53d9e1b1c40ab9cf880c93bacd2d5e3bf5fad9a
SyandyArda <syandi.2901@gmail.com>
2024-05-31 11:14:39 +0700

    circular queues done

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -132,11 +132,16 @@ int main() {
 				return 0;
 			}
 			default: {
-				cout
+				cout << "Invalid option!!" << endl;
+				break;
 			}
 			}
-				
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
 		}
 	}
+
+	return 0;
 }
 
