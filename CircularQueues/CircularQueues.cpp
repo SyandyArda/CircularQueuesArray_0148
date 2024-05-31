ce2d0221c722eb75eebae9a6c7fd1ed4ebbb06a8
SyandyArda <syandi.2901@gmail.com>
2024-05-31 10:44:51 +0700

    void display

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Queues {
 	int FRONT, REAR, max = 5;
-	int queue_arrau[5];
+	int queue_array[5];
 
 public:
 	Queues() {
@@ -40,5 +40,31 @@ public:
 
 	void remove() {
 		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue underflow\n";
+			return;
+		}
+		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+		// cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			// jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+
+	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+		//cek apakah antrian kosong
 	}
 };
+
