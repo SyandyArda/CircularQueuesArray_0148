2634c0dfd670caa5acb388dc457364ec7b0f3535
SyandyArda <syandi.2901@gmail.com>
2024-05-31 10:26:20 +0700

    commit 2

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -6,4 +6,20 @@ class Queues {
 	int queue_arrau[5];
 
 public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		//Cek apakah antrian penuh
+		if (FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+
+		}
+	}
 };
