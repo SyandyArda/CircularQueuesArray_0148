b8be75dff3725540c1ebfecd962c3f5c57dfbb07
SyandyArda <syandi.2901@gmail.com>
2024-05-31 11:00:16 +0700

    void display done

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -91,9 +91,12 @@ public:
 
 			//iterasi dari awal array hingga rear
 			while (FRONT_position <= REAR_position) {
-
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
 			}
+			cout << endl;
 		}
 	}
 };
+
 
