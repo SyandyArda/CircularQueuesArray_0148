59686d88e0285c6d1bbbf9a24874fec56297b2dc
SyandyArda <syandi.2901@gmail.com>
2024-05-31 10:55:29 +0700

    jika front pst<+ rear pst

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -65,6 +65,21 @@ public:
 		int REAR_position = REAR;
 
 		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+
+		cout << "\nElements in the queue are...\n";
+
+		// jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
 	}
 };
 
